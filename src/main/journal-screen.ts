92c98795f8aa18094e31fcca1f4b574fadaa30a9
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-12-29 14:36:02 -0600

    Add DateDisplay

--- src/main/journal-screen.ts
+++ src/main/journal-screen.ts
@@ -1,30 +1,45 @@
 import P5Lib from 'p5';
 
-import { CanvasScreen, P5Context } from "@batpb/genart";
+import { CanvasContext, CanvasScreen, CoordinateMode, P5Context } from "@batpb/genart";
+import { DateDisplay, DateDisplayConfig } from './date-display';
 
 export class JournalScreen extends CanvasScreen {
     readonly #DATE: Date;
+    readonly #DATE_DISPLAY: DateDisplay;
     
     // #journalEntry: string = '';
     // #username: string = '';
-    #dateString: string = '';
     // #dateGraph: unknown = null;
     
     public constructor() {
         super('journal-screen');
+        const p5: P5Lib = P5Context.p5;
         this.#DATE = new Date();
-        this.#dateString = this.#DATE.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+        
+        const dateDisplayConfig: DateDisplayConfig = {
+            date: this.#DATE,
+            coordinatePosition: p5.createVector(0.1, 0.1),
+            coordinateMode: CoordinateMode.RATIO
+        }
+
+        this.#DATE_DISPLAY = new DateDisplay(dateDisplayConfig);
+        this.addRedrawListener(this.#DATE_DISPLAY);
     }
     
     public draw(): void {
         const p5: P5Lib = P5Context.p5;
-        p5.background(255);
-        p5.fill(0);
-        p5.text(this.#dateString, 100, 100);
+        p5.background(220);
+        this.#DATE_DISPLAY.draw();
     }
 
     public keyPressed(): void {
+        const p5: P5Lib = P5Context.p5;
 
+        if (p5.key === '0') {
+            CanvasContext.updateResolution(720);
+        } else if (p5.key === '9') {
+            CanvasContext.updateResolution(1080);
+        }
     }
 
     public mousePressed(): void {
