4beee8228718c8905d51a9e417ad51f70b07f337
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-12-29 13:53:57 -0600

    Begin work on processing date.

--- src/main/journal-screen.ts
+++ src/main/journal-screen.ts
@@ -1,17 +1,26 @@
-import { CanvasScreen } from "@batpb/genart";
+import P5Lib from 'p5';
+
+import { CanvasScreen, P5Context } from "@batpb/genart";
 
 export class JournalScreen extends CanvasScreen {
-    #journalEntry: string = '';
-    #username: string = '';
+    readonly #DATE: Date;
+    
+    // #journalEntry: string = '';
+    // #username: string = '';
     #dateString: string = '';
-    #dateGraph: unknown = null;
+    // #dateGraph: unknown = null;
     
     public constructor() {
         super('journal-screen');
+        this.#DATE = new Date();
+        this.#dateString = this.#DATE.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
     }
     
     public draw(): void {
-
+        const p5: P5Lib = P5Context.p5;
+        p5.background(255);
+        p5.fill(0);
+        p5.text(this.#dateString, 100, 100);
     }
 
     public keyPressed(): void {
