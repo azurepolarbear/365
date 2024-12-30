7814b7b6d0d12087ebe3f52fa9873bf63f3bc5ac
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-12-29 21:41:10 -0600

    Add fx(hash) project sdk dependency

--- src/fxhash/fxhash.min.d.ts
+++ src/fxhash/fxhash.min.d.ts
@@ -1,79 +1,78 @@
+import { FxHashApi } from "@fxhash/project-sdk";
+
 interface Window {
-    $fx: {
-        rand: () => number;
-        params: (params: FxParameter[]) => void;
-    };
+    $fx: FxHashApi;
 }
 
-declare type FxParameter = NumberParameter | StringParameter | BooleanParameter | ColorParameter | SelectParameter | BigIntParameter | BytesParameter;
+// declare type FxParameter = NumberParameter | StringParameter | BooleanParameter | ColorParameter | SelectParameter | BigIntParameter | BytesParameter;
 
-// a parameter definition
-declare interface Parameter {
-    id: string;
-    name?: string;
-    type: 'number' | 'string' | 'boolean' | 'color' | 'select' | 'bigint' | 'bytes';
-    default?: string | number | bigint | boolean;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-    options?: { min: number; max: number; step?: number; } | { minLength: number; maxLength: number; } | { options: string[]; } | { length: number; };
-}
+// // a parameter definition
+// declare interface Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'number' | 'string' | 'boolean' | 'color' | 'select' | 'bigint' | 'bytes';
+//     default?: string | number | bigint | boolean;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+//     options?: { min: number; max: number; step?: number; } | { minLength: number; maxLength: number; } | { options: string[]; } | { length: number; };
+// }
 
-declare interface NumberParameter extends Parameter {
-    id: string;
-    name?: string;
-    type: 'number';
-    default?: number;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-    options?: { min: number; max: number; step?: number; };
-}
+// declare interface NumberParameter extends Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'number';
+//     default?: number;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+//     options?: { min: number; max: number; step?: number; };
+// }
 
-declare interface StringParameter extends Parameter {
-    id: string;
-    name?: string;
-    type: 'string';
-    default?: string;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-    options?: { minLength: number; maxLength: number; };
-}
+// declare interface StringParameter extends Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'string';
+//     default?: string;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+//     options?: { minLength: number; maxLength: number; };
+// }
 
-declare interface BooleanParameter extends Parameter {
-    id: string;
-    name?: string;
-    type: 'boolean';
-    default?: string;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-}
+// declare interface BooleanParameter extends Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'boolean';
+//     default?: string;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+// }
 
-declare interface ColorParameter extends Parameter {
-    id: string;
-    name?: string;
-    type: 'color';
-    default?: string;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-}
+// declare interface ColorParameter extends Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'color';
+//     default?: string;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+// }
 
-declare interface BytesParameter extends Parameter {
-    id: string;
-    name?: string;
-    type: 'bytes';
-    default?: string;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-    options?: { length: number; };
-}
+// declare interface BytesParameter extends Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'bytes';
+//     default?: string;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+//     options?: { length: number; };
+// }
 
-declare interface SelectParameter extends Parameter {
-    id: string;
-    name?: string;
-    type: 'select';
-    default?: string;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-    options?: { options: string[]; };
-}
+// declare interface SelectParameter extends Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'select';
+//     default?: string;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+//     options?: { options: string[]; };
+// }
 
-declare interface BigIntParameter extends Parameter {
-    id: string;
-    name?: string;
-    type: 'bigint';
-    default?: number;
-    update?: 'page-reload' | 'sync' | 'code-driven';
-    options?: { min: number; max: number; };
-}
+// declare interface BigIntParameter extends Parameter {
+//     id: string;
+//     name?: string;
+//     type: 'bigint';
+//     default?: number;
+//     update?: 'page-reload' | 'sync' | 'code-driven';
+//     options?: { min: number; max: number; };
+// }
